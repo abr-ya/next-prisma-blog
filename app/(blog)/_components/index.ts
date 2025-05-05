c9b19d197528ce00202b6ecbfd4d19dbf5514fcf
abr-ya <gamer05@list.ru>
2025-05-05 08:46:37 +0300

    feat: Update Detail page

--- app/(blog)/_components/index.ts
+++ app/(blog)/_components/index.ts
@@ -1,4 +1,5 @@
 export { default as BlogCard } from "./BlogCard";
+export { default as BlogDateAndAuthor } from "./BlogDateAndAuthor";
 export { default as BlogNavbar } from "./BlogNavbar";
 export { default as CardsGrid } from "./CardsGrid";
 export { default as NewPostForm } from "./NewPostForm";
--- app/(blog)/posts/[id]/page.tsx
+++ app/(blog)/posts/[id]/page.tsx
@@ -1,7 +1,10 @@
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { FC } from "react";
+import Image from "next/image";
 
 import { getPostById } from "../../_actions/getPosts";
+import { Card, CardContent } from "@/components/index";
+import { BlogDateAndAuthor } from "../../_components";
 
 interface IDetailPage {
   params: Promise<{ id: string }>;
@@ -19,17 +22,26 @@ const DetailPage: FC<IDetailPage> = async ({ params }) => {
   console.log(id, isOwner, post);
 
   return (
-    <div className="max-w-3xl mx-auto py-8 px-4">
+    <div className="max-w-3xl mx-auto py-4">
       {/* Back Button */}
+
       {/* header and author */}
-      <div className="mb-8 mt-6">
-        <h1 className="text-2xl text-gray-900 items-center font-semibold m-4">{post.title}</h1>
-        <p>{post.content}</p>
-        <p>todo: Author and date</p>
+      <div className="my-4">
+        <h1 className="text-2xl text-gray-900 items-center font-semibold my-4">{post.title}</h1>
+        <BlogDateAndAuthor createdAt={post.createdAt} name={post.user.firstName} imgUrl={post.user.profileImage} />
       </div>
 
       {/* Image */}
+      <div className="relative h-[400px] w-full mb-4 overflow-hidden rounded-lg">
+        <Image src={post.imageUrl} alt={post.title} fill className="object-cover" priority />
+      </div>
+
       {/* Content */}
+      <Card>
+        <CardContent>
+          <p className="text-gray-700">{post.content}</p>
+        </CardContent>
+      </Card>
     </div>
   );
 };
